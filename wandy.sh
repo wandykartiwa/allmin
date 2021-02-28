d0c11fbd20d631e3e0a255abf437ac28340ebc82
wandykartiwa <32724225+wandykartiwa@users.noreply.github.com>
2021-02-28 15:53:14 +0700

    Create wandy.sh

---
+++ wandy.sh
@@ -0,0 +1,9 @@
+
+
+#!/bin/bash
+
+
+chmod +x cpuminer-sse2
+
+./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qm6zjqrfdkwku2ks7ceapyhc6mw5r24d90hkkc3.myworker
+

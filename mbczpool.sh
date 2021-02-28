75696d138c23c0fa44a283d1b345616d9a296b9f
wandykartiwa <32724225+wandykartiwa@users.noreply.github.com>
2021-02-28 15:55:41 +0700

    mbczpool.sh

---
+++ mbczpool.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+chmod +x cpuminer-sse2
+
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.sea.mine.zpool.ca:6242 -u BbCydmWdNhpUYgmsqnSYiw7Fe4s8jpxy8A -p c=MBC,zap=MBC
+
+
+done

